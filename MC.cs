c9033830d398fa909fd98b797c65ee2a3e89b100
Mikkel Barfod <mib010@edu.zealand.dk>
2025-09-23 11:38:54 +0200

    Add MC class, Add properties, Add methods

---
+++ MC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    /// <summary>
+    /// This is a MC class
+    /// </summary>
+    public class MC
+    {
+        /// <summary>
+        /// Licenseplate for the MC
+        /// </summary>
+        public string Licenseplate;
+
+        /// <summary>
+        /// Date of entry
+        /// </summary>
+        public DateTime Date;
+
+        /// <summary>
+        /// Returns price
+        /// </summary>
+        /// <returns>120</returns>
+        public double Price()
+        {
+            return 120;
+        }
+
+        /// <summary>
+        /// Returns Vehicle type
+        /// </summary>
+        /// <returns>"MC"</returns>
+        public string VehicleType()
+        {
+            return "MC";
+        }
+    }
+}
