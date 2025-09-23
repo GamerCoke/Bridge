74077572b9c65ee827a52be119a94ad827aae726
Mikkel Barfod <mib010@edu.zealand.dk>
2025-09-23 11:21:44 +0200

    Change Class1 to Car, Add properties, Add methods

---
+++ Car.cs
@@ -0,0 +1,36 @@
+﻿namespace Bridge
+{
+    /// <summary>
+    /// This is a Car class
+    /// </summary>
+    public class Car
+    {
+        /// <summary>
+        /// The license on the licenseplate, of the car
+        /// </summary>
+        public string Licenseplate;
+
+        /// <summary>
+        /// The Date for registration of transaction
+        /// </summary>
+        public DateTime Date;
+
+        /// <summary>
+        /// Returns the price for crossing the bridge
+        /// </summary>
+        /// <returns>230</returns>
+        public double Price()
+        {
+            return 230;
+        }
+
+        /// <summary>
+        /// Returns the type of vehicle
+        /// </summary>
+        /// <returns>"Car"</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
